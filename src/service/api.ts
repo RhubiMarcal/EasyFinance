3e6ad5383efc16f05fc928bfed7c3894bbbef271
Kaique <kaiquesimoes58@gmail.com>
2025-06-11 11:12:43 -0300

    Bugfix

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,8 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const baseAPI = createApi({
   baseQuery: fetchBaseQuery({
-    // baseUrl: 'https://easyfinanceapi-backend-production.up.railway.app/',
-    baseUrl: 'http://127.0.0.1:8000/',
+    baseUrl: 'https://easyfinanceapi-backend-production.up.railway.app/',
     credentials: 'include'
   }),
   tagTypes: ['Historico', 'Categoria', 'Usuario', 'Goal', 'Limit'],
